fb4ad5fbd545db3a49c920d3c13dd19d454b3ae4
Benjamin Trigona-Harany <bosth@alumni.sfu.ca>
2012-12-08 09:11:16 -0800

    natural earth with postgis

---
+++ workspaces/ne_pg/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TEST=$1
+NODES=$2
+THREADS=$3
+LOOPS=$4
+
+if [ "${TEST}" = "wms_tiled" ]; then
+  jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=tiles -Jheight=256 -Jwidth=256
+elif [ "${TEST}" = "wms_untiled" ]; then
+  jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=bboxes -Jheight=1024 -Jwidth=1024
+else
+  echo "Unknown test"
+  exit 1
+fi
