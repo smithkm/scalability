11098126b44e683d8e9d93200b26462426a17b0a
Benjamin Trigona-Harany <bosth@alumni.sfu.ca>
2012-12-31 12:39:47 -0800

    pyramid for raster

--- workspaces/ne_raster/run.sh
+++ workspaces/ne_raster/run.sh
@@ -6,10 +6,9 @@ THREADS=$3
 LOOPS=$4
 
 if [ "${TEST}" = "wms_tiled" ]; then
-  jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=tiles -Jheight=256 -Jwidth=256
-elif [ "${TEST}" = "wms_tiled_im" ]; then
-  exit 1
-  #jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=tiles -Jheight=256 -Jwidth=256 -Jlayer=NE2_HR_LC_SR_W_DR_imagemosaic
+  jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=worldtiles -Jheight=256 -Jwidth=256
+elif [ "${TEST}" = "wms_tiled_pyramid" ]; then
+  jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=worldtiles -Jheight=256 -Jwidth=256 -Jlayer=NE2_HR_LC_SR_W_DR_pyramid
 else
   echo "Unknown test"
   exit 1
