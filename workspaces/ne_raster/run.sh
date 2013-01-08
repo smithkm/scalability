4c6a465f8337389395f4b3f605bc36e7466e463d
Benjamin Trigona-Harany <bosth@alumni.sfu.ca>
2013-01-07 16:39:59 -0800

    run.sh for raster w/o overview

--- workspaces/ne_raster/run.sh
+++ workspaces/ne_raster/run.sh
@@ -7,6 +7,8 @@ LOOPS=$4
 
 if [ "${TEST}" = "wms_tiled" ]; then
   jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=tiles -Jheight=256 -Jwidth=256
+elif [ "${TEST}" = "wms_tiled_noover" ]; then
+  jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=tiles -Jheight=256 -Jwidth=256 -Jlayer=NE2_HR_LC_SR_W_DR_noover
 elif [ "${TEST}" = "wms_tiled_pyramid" ]; then
   jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=tiles -Jheight=256 -Jwidth=256 -Jlayer=NE2_HR_LC_SR_W_DR_pyramid
 else
