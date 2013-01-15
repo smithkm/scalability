44e20cb65c95f4718b2b58e81b44698c1b0893b1
Benjamin Trigona-Harany <bosth@opengeo.org>
2013-01-15 11:34:06 -0800

    wfs-t test

--- workspaces/wfs_t/run.sh
+++ workspaces/wfs_t/run.sh
@@ -6,8 +6,8 @@ NODES=$2
 THREADS=$3
 LOOPS=$4
 
-if [ "${TEST}" = "wfs-t" ]; then
-  exit 1
+if [ "${TEST}" = "wfs_t" ]; then
+  jmeter -p ../jmeter.properties -t wfs_t.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jserver=scale.dev.opengeo.org -Jport=80 -Jdbserver=scale-db.dev.opengeo.org
 else
   echo "Unknown test"
   exit 1
