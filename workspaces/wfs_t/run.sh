219496d825cbb47d1d75e720561aa8b0fb6d307d
Benjamin Trigona-Harany <bosth@opengeo.org>
2013-01-16 18:41:00 +0000

    don't use stdin inserts in jmeter JDBC

--- workspaces/wfs_t/run.sh
+++ workspaces/wfs_t/run.sh
@@ -6,7 +6,7 @@ NODES=$2
 THREADS=$3
 LOOPS=$4
 
-jmeter -p ../jmeter.properties -t dbsetup.jmx -n
+jmeter -p ../jmeter.properties -t dbsetup.jmx -n -Jdbserver=scale-db.dev.opengeo.org
 
 if [ "${TEST}" = "wfs_t" ]; then
   jmeter -p ../jmeter.properties -t wfs_t.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jserver=scale.dev.opengeo.org -Jport=80 -Jdbserver=scale-db.dev.opengeo.org
