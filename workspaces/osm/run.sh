16c88b5938c51eb15d7e041ffe2d4b24ff1b56a7
Benjamin Trigona-Harany <bosth@opengeo.org>
2013-03-06 23:55:18 +0000

    test any arbitrary osm layer

--- workspaces/osm/run.sh
+++ workspaces/osm/run.sh
@@ -9,6 +9,9 @@ if [ "${TEST}" = "wms_tiled" ]; then
   jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=tiles -Jheight=256 -Jwidth=256
 elif [ "${TEST}" = "wms_untiled" ]; then
   jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=bboxes -Jheight=1024 -Jwidth=1024
+elif [[ "${TEST}" = wms_tiled_* ]]; then
+  # the test name specifies a layer
+  jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=tiles -Jheight=256 -Jwidth=256 -Jlayer=${TEST:10}
 else
   echo "Unknown test"
   exit 1
