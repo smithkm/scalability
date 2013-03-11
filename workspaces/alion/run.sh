7bec5597cb134f0ee2a42213667379709e0a9b1d
Benjamin Trigona-Harany <bosth@opengeo.org>
2013-03-11 12:54:52 -0700

    add WCS test, remove nitf bbox errors

--- workspaces/alion/run.sh
+++ workspaces/alion/run.sh
@@ -11,6 +11,10 @@ if [[ "${TEST}" = geotiff* ]]; then
   jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=geotiff -Jsrs=32642 -Jheight=256 -Jwidth=256 -Jlayer=${TEST} -Jserver=$5 -Jport=$6
 elif [ "${TEST}" = "nitf" ]; then
   jmeter -p ../jmeter.properties -t wms.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=nitf    -Jsrs=4326  -Jheight=256 -Jwidth=256 -Jlayer=${TEST} -Jserver=$5 -Jport=$6
+elif [[ "${TEST}" = wcs_geotiff* ]]; then
+  jmeter -p ../jmeter.properties -t wcs.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=geotiff -Jsrs=32642 -Jheight=256 -Jwidth=256 -Jlayer=${TEST} -Jserver=$5 -Jport=$6
+elif [ "${TEST}" = "wcs_nitf" ]; then
+  jmeter -p ../jmeter.properties -t wcs.jmx -n -Jnodes=${NODES} -Jthreads=${THREADS} -Jloops=${LOOPS} -Jbboxes=nitf    -Jsrs=4326  -Jheight=256 -Jwidth=256 -Jlayer=${TEST} -Jserver=$5 -Jport=$6
 else
   echo "Unknown test"
   exit 1
